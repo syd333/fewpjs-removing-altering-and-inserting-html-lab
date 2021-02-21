4b70f9b549922dbe31550e68ac8e443d3663ffcd
syd333 <sydmiranda@icloud.com>
2021-02-21 14:56:32 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove()
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = "victory"
+newHeader.innerHTML = "David Medina is the champion!";
